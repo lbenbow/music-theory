dbc5cd522ebc55b59646453c92227d4e3ce73a8b
Lawrence Benbow <lawrence.benbow@gmail.com>
2019-08-23 15:04:22 -0400

    Added scratch.py

---
+++ scratch.py
@@ -0,0 +1,11 @@
+import sys
+import support
+
+#my_key = Key(key_signature='A')
+key = getkeysig('A')
+exit(0)
+print(f'Key = {key}')
+# print(f'Returned key {sentKey} = ', key)
+
+# print(f"{support.keySig['a']}")
+
